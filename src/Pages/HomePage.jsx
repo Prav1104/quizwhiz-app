a99752a1efdea1840868048bd60be38aaeec5242
Praveen Nitharwal <praveennitharwal55@gmail.com>
2025-05-04 22:36:23 +0530

    added search bar

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { BrainCircuit } from 'lucide-react';
+import React, { useState } from 'react';
+import SearchBar from '../components/quiz/SearchBar.jsx'
+import './HomePage.css'
+
+
 
 function Home(){
     return (
         <div>
-            This is Home Page
+            <div className='landing-page'>
+                <div className='landing-container'>
+                    <div className='landing-header'>
+                        <div className='landing-logo'>
+                            <h1><BrainCircuit size={'5rem'}/>QuizWhiz</h1>
+                        </div>
+                        <p className='landing-tagline'>Test your tech knowledge with interactive quizzes..!</p>
+                        <SearchBar/>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
